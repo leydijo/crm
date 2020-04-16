311e6b4b413e533bc89df3cf95ec6e7e2af30abc
Leydi lenis Leydijo <leydilenisleydijo@Leydis-MacBook-Pro.local>
2020-04-15 20:11:36 -0500

    esqueleto de factura dos

--- resources/views/ventas/ventasPdf.blade.php
+++ resources/views/ventas/ventasPdf.blade.php
@@ -68,7 +68,81 @@
                         </tr>
                 
                     </thead>
+                    <tbody>
+                        
+
+                        <tr>
+                            <td><div class="lineaCliente"></div></td>
+                        </tr>
+                        
+                        <tr>
+                            <td>Nombre:</td>
+                        </tr>
+                        <tr>
+                            <td>identificación:</td>
+                        </tr>
+                        <tr> 
+                            <td>Dirección:</td> 
+                        </tr>
+                        <tr> 
+                            <td>Telefono:</td> 
+                        </tr>
+                        <tr> 
+                            <td>Email:</td> 
+                        </tr>
+                     
+                    </tbody>
                     
+                </table>
+               
+            </section>
+
+            <section class="completar">
+
+                <table >
+                    <thead>
+                        <tr>
+                            <th ></th>
+                            
+                            
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($orders as $key => $order)
+
+                        <tr>
+                            <td></td>
+                        </tr>
+                        
+                        <tr>
+                            <td>{{$order->new_user->fullname}}</td>
+                        </tr>
+                        <tr>
+                            <td>{{$order->new_user->identification}}</td>
+                        </tr>
+                        <tr> 
+                            <td>{{$order->new_user->address}}</td> 
+                        </tr>
+                        <tr> 
+                            <td>{{$order->new_user->phone}}</td> 
+                        </tr>
+                        <tr> 
+                            <td>{{$order->new_user->email}}</td> 
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </section>
+
+            <section class="cliente">
+
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Establecimiento</th>
+                            
+                        </tr>
+                    </thead>
                     <tbody>
                         <tr>
                             <td><div class="linea"></div></td>
@@ -78,32 +152,14 @@
                         </tr>
                         <tr>
                             <td>Cll 69a # 4-12</td>
-                           
-                        </tr>
-                        <tr></tr>
-                    </tbody>
-                </table>
-               
-            </section>
-
-
-            <section class="cliente">
-
-                <table >
-                    <thead>
-                        <tr>
-                            <th >Enviar a:</th>
-                            
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>Cleanrabbit</td>
                         </tr>
                         <tr>
-                            <td>Cll 69a # 4-12</td>
+                            <td>5440588</td>
                         </tr>
-                        <tr></tr>
+                        <tr>
+                            <td>info@cleanrabit.com.co</td>
+                        </tr>
+                       
                     </tbody>
                 </table>
             </section>
@@ -111,10 +167,62 @@
 
             
         </div>
+        <section class="margen">
+            <table class="table table-bordered ">
+                <thead class="tabla">
+                  <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Descripción</th>
+                    <th scope="col">Precio Unitario</th>
+                    <th scope="col">Precio/th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+            </table>
+            <table class=" table-bordered ">
+                <tbody>
+                    <tr>
+                        <td scope="col">a-f</td>
+                    </tr>
+                    <tr>
+                        <td>a-f</td>
+                    </tr>
+                    <tr>
+                        <td>a-f</td>
+                    </tr>
+                </tbody>
+            </table>
+            
+        </section>
+        <div class="di">r</div>
+        <div class="di">r</div>
+        <div class="di">r</div>
+        <div class="di">r</div>
+        <div class="di">r</div>
     </div>
     
     
+<footer>
 
-    
+</footer>
+     <div class="line"></div>
 </body>
 </html>
